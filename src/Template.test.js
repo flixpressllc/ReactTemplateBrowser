72675281cfeb7c68ce69ef5bb812330c7b34e328
Don Denton <don@happycollision.com>
2016-11-02 16:11:40 -0500

    Fix improper object testing...

--- src/Template.test.js
+++ src/Template.test.js
@@ -2,20 +2,27 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Template from './Template';
 
-const VALID_TEMPLATE_OBJECT = {
-  id: 79,
-  tags: ['pro', 'gags'],
-  name: 'Sparky Pro Electrocutionator',
-  type: 'TextOnly',
-  image: 'some/image.jpg'
+function createTemplate() {
+  const VALID_TEMPLATE_OBJECT = {
+    id: 79,
+    tags: ['pro', 'gags'],
+    name: 'Sparky Pro Electrocutionator',
+    type: 'TextOnly',
+    image: 'some/image.jpg',
+    duration: '0:23',
+    plan: 'Expert',
+    price: '$5'
+  };
+
+  return VALID_TEMPLATE_OBJECT;
 }
 
 it('renders without crashing', () => {
- shallow(<Template template={ VALID_TEMPLATE_OBJECT } />);
+ shallow(<Template template={ createTemplate() } />);
 });
 
 it('renders the template name', () => {
-  let templateObject = VALID_TEMPLATE_OBJECT;
+  let templateObject = createTemplate();
   templateObject.name = 'Sparky'
   
   const component = shallow(<Template template={ templateObject } />)
@@ -24,7 +31,7 @@ it('renders the template name', () => {
 });
 
 it('creates a correctly formatted link', () => {
-  let templateObject = VALID_TEMPLATE_OBJECT;
+  let templateObject = createTemplate();
   templateObject.id = '79';
   templateObject.type = 'Slides';
   
@@ -34,7 +41,7 @@ it('creates a correctly formatted link', () => {
 });
 
 it('displays an image thumbnail', () => {
-  let templateObject = VALID_TEMPLATE_OBJECT;
+  let templateObject = createTemplate();
   templateObject.image = 'an/image.jpg'
   
   const component = shallow(<Template template={ templateObject } />);
