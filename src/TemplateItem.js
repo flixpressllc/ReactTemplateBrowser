fb36343ed43f13dbd7f7778c0ecb1790ca9237d2
Don Denton <don@happycollision.com>
2016-11-02 15:41:18 -0500

    Templates generate links for editing

--- src/TemplateItem.js
+++ src/TemplateItem.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 class TemplateItem extends Component {
+  
+  createLink() {
+    const t = this.props.item;
+    return `/templates/${t.type}.aspx?tid=${t.id}`;
+  }
+  
   render(){
+    const link = this.createLink();
+    const template = this.props.item;
     return (
       <div className='template'>
-        <header>{ this.props.item.name }</header>
+        <header>{ template.name }</header>
+        <a href={ link }>Click here</a>
       </div>
     );
   }
--- src/TemplateItem.test.js
+++ src/TemplateItem.test.js
@@ -5,7 +5,8 @@ import TemplateItem from './TemplateItem';
 const VALID_TEMPLATE_ITEM = {
   id: 79,
   tags: ['pro', 'gags'],
-  name: 'Sparky Pro Electrocutionator'
+  name: 'Sparky Pro Electrocutionator',
+  type: 'TextOnly'
 }
 
 it('renders without crashing', () => {
@@ -13,6 +14,20 @@ it('renders without crashing', () => {
 });
 
 it('renders the template name', () => {
-  const component = shallow(<TemplateItem item={ VALID_TEMPLATE_ITEM } />)
-  expect(component.text()).toContain(VALID_TEMPLATE_ITEM.name)
+  let templateObject = VALID_TEMPLATE_ITEM;
+  templateObject.name = 'Sparky'
+  
+  const component = shallow(<TemplateItem item={ templateObject } />)
+  
+  expect(component.text()).toContain('Sparky');
+});
+
+it('creates a correctly formatted link', () => {
+  let templateObject = VALID_TEMPLATE_ITEM;
+  templateObject.id = '79';
+  templateObject.type = 'Slides';
+  
+  const component = shallow(<TemplateItem item={ templateObject } />);
+
+  expect(component.find('a').first().prop('href')).toBe('/templates/Slides.aspx?tid=79');
 });
