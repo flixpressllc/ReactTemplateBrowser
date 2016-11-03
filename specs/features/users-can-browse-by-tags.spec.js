0317ebe5c15c6ba055c1203d9ea742a51a1489b9
Don Denton <don@happycollision.com>
2016-11-03 16:03:46 -0500

    Using describe block for feature specs

--- specs/features/users-can-browse-by-tags.spec.js
+++ specs/features/users-can-browse-by-tags.spec.js
@@ -3,39 +3,41 @@ import { mount } from 'enzyme';
 import Browser from '../../src/components/Browser';
 import create from '../spec-helpers/FaketoryGirl';
 
-it('creates a tag pane with the right number of nodes', () => {
-  const templates = [ 
-    create('template', {tags: ['one', 'two']}),
-    create('template', {tags: ['one', 'two', 'three']})
-  ];
-  const app = mount(<Browser templates={templates} />);
-  let nodeLength = app.find('TagPane .tag').length;
-  
-  expect(nodeLength).toBe(3);
-});
+describe('Feature: Users can browse by tag', () => {
+  it('creates a tag pane with the right number of nodes', () => {
+    const templates = [ 
+      create('template', {tags: ['one', 'two']}),
+      create('template', {tags: ['one', 'two', 'three']})
+    ];
+    const app = mount(<Browser templates={templates} />);
+    let nodeLength = app.find('TagPane .tag').length;
+    
+    expect(nodeLength).toBe(3);
+  });
 
-it('changes displayed templates on tag click', () => {
-  const templates = [ 
-    create('template', {tags: ['green']}),
-    create('template', {tags: ['red']})
-  ];
-  const app = mount(<Browser templates={ templates } />);
-  const firstTag = app.find('TagPane .tag').at(1);
+  it('changes displayed templates on tag click', () => {
+    const templates = [ 
+      create('template', {tags: ['green']}),
+      create('template', {tags: ['red']})
+    ];
+    const app = mount(<Browser templates={ templates } />);
+    const firstTag = app.find('TagPane .tag').at(1);
 
-  firstTag.simulate('click');
+    firstTag.simulate('click');
 
-  expect(app.find('Template').length).toBe(1);
-});
+    expect(app.find('Template').length).toBe(1);
+  });
 
-it('displays all templates on "all" tag click', () => {
-  const templates = [ 
-    create('template', {tags: ['green']}),
-    create('template', {tags: ['red']})
-  ];
-  const app = mount(<Browser templates={ templates } />);
-  const firstTag = app.find('TagPane a.tag-all').first();
+  it('displays all templates on "all" tag click', () => {
+    const templates = [ 
+      create('template', {tags: ['green']}),
+      create('template', {tags: ['red']})
+    ];
+    const app = mount(<Browser templates={ templates } />);
+    const firstTag = app.find('TagPane a.tag-all').first();
 
-  firstTag.simulate('click');
+    firstTag.simulate('click');
 
-  expect(app.find('Template').length).toBe(2);
+    expect(app.find('Template').length).toBe(2);
+  });
 });
--- specs/features/users-can-browse-with-filters.spec.js
+++ specs/features/users-can-browse-with-filters.spec.js
@@ -3,17 +3,21 @@ import { mount } from 'enzyme';
 import Browser from '../../src/components/Browser';
 import create from '../spec-helpers/FaketoryGirl';
 
-it('changes displayed cost structure on switch click', () => {
-  const templates = [ 
-    create('template', {price: '$45', plan: 'Personal'})
-  ];
-  const app = mount(<Browser templates={ templates } />);
-  const pricesSwitch = app.find('CostSwitch [value="price"]');
-  const plansSwitch = app.find('CostSwitch [value="plan"]');
+describe('Feature: Users can browse with filters', () => {
+  describe('while logged out', () => {
+    it('changes displayed cost structure on switch click', () => {
+      const templates = [ 
+        create('template', {price: '$45', plan: 'Personal'})
+      ];
+      const app = mount(<Browser templates={ templates } />);
+      const pricesSwitch = app.find('CostSwitch [value="price"]');
+      const plansSwitch = app.find('CostSwitch [value="plan"]');
 
-  pricesSwitch.simulate('change');
-  expect(app.find('Template').first().text()).toContain('$45');
+      pricesSwitch.simulate('change');
+      expect(app.find('Template').first().text()).toContain('$45');
 
-  plansSwitch.simulate('change');
-  expect(app.find('Template').first().text()).toContain('Personal');
+      plansSwitch.simulate('change');
+      expect(app.find('Template').first().text()).toContain('Personal');
+    });
+  });
 });
