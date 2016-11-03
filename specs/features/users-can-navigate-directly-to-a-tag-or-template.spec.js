134735d0c538d7794659f9d59f65bf2fefb26b47
Don Denton <don@happycollision.com>
2016-11-03 16:09:34 -0500

    Add pending spec Andrey suggested

---
+++ specs/features/users-can-navigate-directly-to-a-tag-or-template.spec.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Browser from '../../src/components/Browser';
+import create from '../spec-helpers/FaketoryGirl';
+
+describe('Feature: Users can navigate directly to a tag or template', () => {
+  describe('when the url contains a tag name', () => {
+    it('sets the chosen tag based on the url', () => {
+      pending();
+    });
+  });
+  describe('when the url contains a template id', () => {
+    it('shows a view containing that template and opens the colorbox to that template', () => {
+      pending();
+    });
+  });
+});
