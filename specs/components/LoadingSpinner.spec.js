56417d8500f33a17825a0260fd88596fc501c1fd
Don Denton <don@happycollision.com>
2016-11-09 13:47:58 -0600

    Adds loading spinner component (unimplemented)

---
+++ specs/components/LoadingSpinner.spec.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import LoadingSpinner from '../../src/components/LoadingSpinner';
+
+describe('LoadingSpinner', () =>{
+  it('renders without crashing', () => {
+  shallow(<LoadingSpinner />);
+  });
+
+  it('appears when active', () => {
+    const component = shallow(<LoadingSpinner active={ true } />);
+    expect(component.find('.reactTemplateBrowser-LoadingSpinner-spinner').length).toBe(1);
+  });
+
+  it('disappears when inactive', ()=>{
+    const component = shallow(<LoadingSpinner active={ false } />);
+    expect(component.find('.reactTemplateBrowser-LoadingSpinner-spinner').length).toBe(0);
+  });
+
+  it('assumes it is active if no props are passed in', () =>{
+    const component = shallow(<LoadingSpinner />);
+    expect(component.find('.reactTemplateBrowser-LoadingSpinner-spinner').length).toBe(1);
+  });
+});
---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import cx from 'classnames';
+import '../css/LoadingSpinner.css';
+
+class LoadingSpinner extends Component {
+  
+  render(){
+    let spinner;
+    if (!!this.props.active) {
+      spinner = <img className='reactTemplateBrowser-LoadingSpinner-spinner' src='https://flixpress.com/images/ajax-loader.gif'/>
+    } else {
+      spinner = null;
+    }
+    
+    return (
+      <div className='reactTemplateBrowser-LoadingSpinner'>
+        { spinner }
+      </div>
+    );
+  }
+}
+
+LoadingSpinner.defaultProps = {
+  active: true
+}
+
+export default LoadingSpinner;
