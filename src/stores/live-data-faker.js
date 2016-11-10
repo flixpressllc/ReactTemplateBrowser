e1bbb2cad955a713910608bf12acc804a47633d4
Don Denton <don@happycollision.com>
2016-11-10 16:26:38 -0600

    Much better faek data via copy paste from SQL result

--- src/stores/live-data-faker.js
+++ src/stores/live-data-faker.js
@@ -1,168 +1,85 @@
-const templateData = [
-  // {
-  //   name: 'NAME',
-  //   price: 500,
-  //   duration: 27,
-  //   plan: 'Personal',
-  //   type: TYPE,
-  //   has4k: false
-  // },
-  {
-    name: 'Lots of Text',
-    price: 0,
-    duration: 25,
-  },
-  {
-    name: 'World Intro',
-    price: 0,
-    duration: 10,
-  },
-  {
-    name: 'Tech puzzle',
-    price: 6,
-    duration: 52,
-    plan: 'Personal',
-    type: 'Images',
-  },
-  {
-    name: 'Envelope Photos',
-    price: 5,
-    duration: 66,
-    plan: 'Personal',
-    type: 'Images',
-  },
-  {
-    name: 'Clean Impact',
-    price: 6,
-    duration: 40,
-    plan: 'Personal',
-    type: 'Images',
-  },
-  {
-    name: 'Christmas Tree',
-    price: 6,
-    duration: 60,
-    plan: 'Personal',
-    type: 'Images',
-    id: 7
-  },
-  {
-    name: 'Art Gallery',
-    price: 6,
-    duration: 66,
-    plan: 'Expert',
-    type: 'Images',
-  },
-  {
-    name: 'Chain Reaction',
-    price: 0,
-    duration: 19,
-    plan: 'Free',
-    type: 'Images',
-    id: 10
-  },
-  {
-    name: 'Space Grid',
-    price: 8,
-    duration: 70,
-    plan: 'Personal',
-    type: 'Images',
-    id: 12
-  },
-  {
-    name: 'Box Shuffle',
-    duration: 15,
-    plan: 'Free',
-    price: 0,
-    type: 'Images',
-  },
-  {
-    name: 'Special Words',
-    duration: 19,
-    plan: 'Personal',
-    price: 4,
-    type: 'Images',
-    id:19
-  },
-  {
-    name: 'Robotic X-form',
-    duration: 16,
-    plan: 'Free',
-    price: 0,
-    type: 'Images',
-  },
-  {
-    name: 'Teams Vs Team',
-    duration: 10,
-    plan: 'Personal',
-    price: 4,
-    type: 'Images',
-    id: 23
-  },
-  {
-    name: 'Cinema Intro',
-    duration: 10,
-    plan: 'Free',
-    price: 0,
-    id: 31
-  },
-  {
-    name: 'Gaming Intro 1',
-    duration: 10,
-    plan: 'Personal',
-    price: 4,
-  },
-  {
-    name: 'Cinema Intro 2',
-    duration: 12,
-    plan: 'Personal',
-    price: 4,
-  },
-  {
-    name: 'Pro Intro 1',
-    duration: 8,
-    plan: 'Personal',
-    price: 4,
-    type: 'Images',
-    id: 35
-  },
-  {
-    name: 'Game Intro 2',
-    duration: 10,
-    plan: 'Personal',
-    price: 6,
-  },
-  {
-    name: 'Falling Words',
-    duration: 15,
-    plan: 'Personal',
-    price: 4,
-  },
-  {
-    name: 'Exploding Block',
-    duration: 10,
-    plan: 'Personal',
-    price: 4,
-  },
+const copyPasteVal = `1	Lots of Text	0	Free Plan Templates	Intros	26 seconds	Free	Images
+2	World Intro	0	Free Plan Templates	Intros	10 seconds	Free	Images
+3	Tech Puzzle	2	Slideshow	Slideshow	52 seconds	Personal	Images
+4	Envelope Photos	1	Slideshow	Slideshow	66 seconds	Personal	Images
+5	Clean Impact	3	Animated Promo	Animated Promo	41 seconds	Personal	Images
+7	Christmas Tree	2	Slideshow	Slideshow	60 seconds	Personal	Images
+8	Art Gallery	2	Slideshow	Slideshow	66 seconds	Expert	Images
+10	Chain Reaction	0	Intros	Intros	19 seconds	Personal	Images
+12	Space Grid	2	Slideshow	Slideshow	70 seconds	Personal	Images
+13	Box Shuffle	0	Intros	Intros	15 seconds	Personal	Images
+19	Special Words	1	Special Occasions	Special Occasions	20 seconds	Personal	Images
+20	Robotic X-form	0	Intros	Intros	17 seconds	Personal	Images
+23	Teams Vs Team	0	Intros	Intros	10 seconds	Personal	Images
+31	Cinema Intro	0	Free Plan Templates	Intros	10 seconds	Free	TextOnly
+32	Gaming Intro 1	1	Intros	Intros	11 seconds	Personal	TextOnly
+33	Cinema Intro 2	1	Intros	Intros	12 seconds	Personal	TextOnly
+35	Pro Intro 1	2	Intros	Intros	8 seconds	Personal	Images
+36	Game Intro 2	1	Intros	Intros	10 seconds	Personal	TextOnly
+37	Falling Words	1	Intros	Intros	15 seconds	Personal	TextOnly
+38	Exploding Block	1	Intros	Intros	10 seconds	Personal	TextOnly
+39	Mining Word	2	Intros	Intros	10 seconds	Personal	TextOnly
+40	Pro Presenter	4	Multi-Layout	Multi-Layout	variable	Expert	Slides
+42	Music Intro	0	Free Plan Templates	Intros	13 seconds	Free	TextOnly
+43	Love Show	1	Special Occasions	Special Occasions	30 seconds	Expert	Images
+44	Game Intro 3	3	Intros	Intros	12 seconds	Personal	TextOnly
+45	TV Shatter 	3	Intros	Intros	10 seconds	Expert	Images
+47	Digits Intro	2	Intros	Intros	10 seconds	Personal	TextOnly
+49	Gaming Intro 4	3	Intros	Intros	11 seconds	Personal	TextOnly
+50	Hands Intro	2	Intros	Intros	8 seconds	Expert	TextOnly
+51	Cubism Intro	2	Intros	Intros	8 seconds	Expert	Images
+53	Digimpact Pro	5	Multi-Layout	Multi-Layout	Variable Timing	Expert	Slides
+54	Sci-Fi Intro	3	Intros	Intros	10 seconds	Personal	Images
+55	Spooky Fest	5	Pranks/Gags	Pranks/Gags	60 seconds	Personal	Images
+56	Gaming XL	5	Intros	Intros	15 Seconds	Personal	Images
+59	Christmas Card	4	Special Occasions	Special Occasions	15 seconds	Personal	Images
+60	Gaming XL2	5	Intros	Intros	15 seconds	Personal	Images
+61	Multi-Person Ad	70	Ads	Ads	30 Seconds	Business	TextOnly
+62	Simplicity	3	Intros	Intros	10 seconds	Personal	TextOnly
+63	Fire Forge Intro	4	Intros	Intros	10 seconds	Expert	TextOnly
+64	Epic Intro	5	Intros	Intros	12 seconds	Expert	Images
+65	Neon Intro	4	Intros	Intros	10 seconds	Expert	TextOnly
+66	Gaming XL3	5	Intros	Intros	10 seconds	Personal	Images
+67	White Angle	3	Intros	Intros	10 seconds	Personal	Images
+68	Pro Solutions Ad	25	Ads	Ads	30 seconds	Expert	Images
+69	BigCity Intro	4	Intros	Intros	10 seconds	Personal	Images
+70	CubePop Intro	3	Intros	Intros	11 seconds	Personal	Images
+71	Explainer ProA	25	Multi-Layout	Multi-Layout	Variable Timing	Business	Slides
+73	Explainer ProB	25	Multi-Layout	Multi-Layout	Variable timing	Business	Slides
+74	Text Customization Demo	0	Intros	Intros	10 seconds	Personal	TrialTextOnly
+75	Image Customization Demo	0	Intros	Intros	10 seconds	Personal	TrialImages
+76	City HQ Intro	8	Intros	Intros	10 seconds	Expert	Images
+77	Glamour Intro	8	Intros	Intros	10 seconds	Personal	Images
+78	Cat Ad Pro	30	Ads	Ads	30 seconds	Expert	TextOnly
+79	Flex Intro	5	Intros	Intros	10 seconds	Expert	TextOnly
+81	CityHQ Pro	35	Multi-Layout	Multi-Layout	Variable Timing	Expert	Slides
+82	Problem Ad	40	Ads	Ads	30 seconds	Business	Images
+83	Stellar Intro	6	Intros	Intros	13 seconds	Expert	TextOnly
+84	Xform Intro	7	Intros	Intros	10 seconds	Expert	TextOnly
+85	Compete Intro	5	Intros	Intros	10 seconds	Expert	Images`
 
-];
+const individualLines = copyPasteVal.match(/[^\r\n]+/g);
+
+function getParams (line) {
+  return line.match(/[^\t]+/g);
+}
 
 let idCounter = 1;
-function createTemplate (name, priceInt, duration, plan, type, tags, has4k, id) {
+function createTemplate (id, name, priceInt, cat1, cat2, duration, plan, type) {
   id = id ? id : idCounter + 1;
   idCounter = id;
   duration = typeof duration === 'string' ? duration : durToSring(duration);
   return {
     id: id,
-    tags: tags || [],
+    tags: [cat1, cat2],
     name: name,
     type: type || 'TextOnly',
     image: 'https://flixpress.com/tempImages/' + id + '.jpg',
-    duration: '0:23',
+    duration: duration,
     plan: plan || 'Free',
     price: '$' + priceInt,
     features: {
-      has4k: !!has4k
+      has4k: false
     }
   }
 };
@@ -177,16 +94,14 @@ function str_pad_left(string,pad,length) {
   return (new Array(length+1).join(pad)+string).slice(-length);
 }
 
-function getTemplate (o) {
-  return createTemplate(o.name, o.price, o.duration, o.plan, o.type, o.tags, o.has4k, o.id);
-}
-
 function create_list () {
   let finalTemplates = [];
-  templateData.forEach(function(element) {
-    finalTemplates.push(getTemplate(element));
+  individualLines.forEach(function(line) {
+    let args = getParams(line);
+    if (args[args.length -1 ].indexOf('Trial') === -1){
+      finalTemplates.push(createTemplate.apply(this, args));
+    }
   }, this);
-  console.log(finalTemplates)
   return finalTemplates;
 }
 
