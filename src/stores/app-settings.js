03ef999dedd645fca7a994f4438792a981bf5276
Don Denton <don@happycollision.com>
2016-11-17 11:32:33 -0600

    Rework app-settings.js along with a spec

--- src/stores/app-settings.js
+++ src/stores/app-settings.js
@@ -6,13 +6,19 @@ export const SUBSCRIPTION_PLAN_NAMES_IN_ORDER = [
   'Enterprise'
 ];
 
-export let SUBSCRIPTION_PLAN_VALUES_HASH = {
-  'Free': SUBSCRIPTION_PLAN_NAMES_IN_ORDER.indexOf('Free'),
-  'Personal': SUBSCRIPTION_PLAN_NAMES_IN_ORDER.indexOf('Personal'),
-  'Expert': SUBSCRIPTION_PLAN_NAMES_IN_ORDER.indexOf('Expert'),
-  'Business': SUBSCRIPTION_PLAN_NAMES_IN_ORDER.indexOf('Business'),
-  'Enterprise': SUBSCRIPTION_PLAN_NAMES_IN_ORDER.indexOf('Enterprise')
+export const PAYG_PLAN_NAMES = [
+  'Pay As You Go',
+  'Reseller'
+]
+
+export const ALL_PLAN_NAMES = [].concat(SUBSCRIPTION_PLAN_NAMES_IN_ORDER, PAYG_PLAN_NAMES);
+
+let planValues = {};
+for (let i = 0; i < SUBSCRIPTION_PLAN_NAMES_IN_ORDER.length; i++) {
+  planValues[SUBSCRIPTION_PLAN_NAMES_IN_ORDER[i]] = i;
 }
+
+export let SUBSCRIPTION_PLAN_VALUES_HASH = planValues;
 
 export const UHD_IMAGE_URL = "/images/4k.jpg";
 
---
+++ src/stores/app-settings.spec.js
@@ -0,0 +1,71 @@
+import * as s from './app-settings';
+
+// These tests just remind us to think through the logic of our
+// other test suites as things change.
+// If any of these ever fail, it is probably a good idea to see
+// where the offending exports are used in our code
+
+const expectedAllPlanNames = [
+  'Free',
+  'Personal',
+  'Expert',
+  'Business',
+  'Enterprise',
+  'Pay As You Go',
+  'Reseller'
+];
+
+const expectedSubscriptionValuesHash = {
+  'Free': 0,
+  'Personal': 1,
+  'Expert': 2,
+  'Business': 3,
+  'Enterprise': 4
+}
+
+it('has a known set of exports', () => {
+  let exports = [];
+  const expectedExports = [
+    'SUBSCRIPTION_PLAN_NAMES_IN_ORDER',
+    'PAYG_PLAN_NAMES',
+    'ALL_PLAN_NAMES',
+    'SUBSCRIPTION_PLAN_VALUES_HASH',
+    'UHD_IMAGE_URL',
+    'DEFAULT_PAGE_SIZE'
+  ];
+
+  for (let name in s) {
+    if (s.hasOwnProperty(name)) { exports.push(name); }
+  }
+
+  for (var i = 0; i < exports.length; i++) {
+    //Reversed Expectation
+    expect(expectedExports).toContain(exports[i]);
+  }
+});
+describe('Expected values', () => {
+  it('has the right values for ALL_PLAN_NAMES', () => {
+    let expectedArray = expectedAllPlanNames;
+    let testingArray = s.ALL_PLAN_NAMES
+
+    for (var i = 0; i < testingArray.length; i++) {
+      expect(expectedArray[i]).toEqual(testingArray[i]);
+    }
+  });
+  it('has the right length of values for ALL_PLAN_NAMES', () => {
+    let expectedArray = expectedAllPlanNames;
+    let testingArray = s.ALL_PLAN_NAMES
+
+    expect(testingArray.length).toEqual(expectedArray.length);
+  });
+  it('has the right values for SUBSCRIPTION_PLAN_VALUES_HASH', () => {
+    let expectedObject = expectedSubscriptionValuesHash;
+    let testingObject = s.SUBSCRIPTION_PLAN_VALUES_HASH
+
+    for (let name in expectedObject) {
+      if (expectedObject.hasOwnProperty(name)){
+        expect(testingObject[name]).toEqual(expectedObject[name]);
+      }
+    } 
+  });
+});
