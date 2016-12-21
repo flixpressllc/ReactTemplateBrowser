a61602de28a6d1efcab89ea358417a74ae3c2300
Don Denton <don@happycollision.com>
2016-12-21 17:17:36 -0600

    get data from file on server

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom';
 import Browser from './components/Browser';
 import DataAdapter from './helpers/DataAdapter';
 
-const tempLiveData = require('./stores/derived-db-data');
+// const tempLiveData = require('./stores/derived-db-data');
 
-const isLocalhost = window.location.hostname === 'localhost';
-const liveDataFlagIsSet = window.location.search.indexOf('live=1') !== -1;
-const shouldUseLiveData = ( !isLocalhost || liveDataFlagIsSet );
+// const isLocalhost = window.location.hostname === 'localhost';
+// const liveDataFlagIsSet = window.location.search.indexOf('live=1') !== -1;
+// const shouldUseLiveData = ( !isLocalhost || liveDataFlagIsSet );
 
-const tabSeparatesTemplates = shouldUseLiveData ? tempLiveData.live : tempLiveData.dev ;
+// const tabSeparatesTemplates = shouldUseLiveData ? tempLiveData.live : tempLiveData.dev ;
 
-const templateData = new DataAdapter(tabSeparatesTemplates);
 
 /*
   Required options:
@@ -23,12 +22,18 @@ const templateData = new DataAdapter(tabSeparatesTemplates);
     userType: (string) one of 'guest', 'Free', 'Personal', 'Expert', 'Business', 'Enterprise', 'Pay As You Go', 'Reseller'
 */
 function initTemplateBrowser (options) {
-  ReactDOM.render(
-    <Browser templates={ templateData.getAll() }
-      userType={ options.userType }
-      onTemplateOpen={ options.onTemplateOpen } />,
-    document.getElementById(options.divId)
-  );
+  window.$.ajax({
+    url:'/Scripts/ReactTemplateBrowser/liveTemplateData.txt',
+    dataType: 'text'
+  }).done(function (data) {
+    const templateData = new DataAdapter(data);
+    ReactDOM.render(
+      <Browser templates={ templateData.getAll() }
+        userType={ options.userType }
+        onTemplateOpen={ options.onTemplateOpen } />,
+      document.getElementById(options.divId)
+    );
+  })
 }
 
 window.initTemplateBrowser = initTemplateBrowser;
