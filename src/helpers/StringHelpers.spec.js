cb647a1bd06af1e23192571a8a48e7a83fecdbe2
Don Denton <don@happycollision.com>
2016-12-27 14:14:19 -0600

    Adds unslugify string helper

--- src/helpers/StringHelpers.spec.js
+++ src/helpers/StringHelpers.spec.js
@@ -1,4 +1,4 @@
-import { slugify, padLeft } from '../../src/helpers/StringHelpers';
+import { slugify, unslugify, padLeft } from '../../src/helpers/StringHelpers';
 
 describe('slugify', () => {
   it('replaces spaces with hyphens', () => {
@@ -30,6 +30,24 @@ end`;
   it('does not overload hyphens', () => {
     const str = 'simple---text';
     expect(slugify(str)).toBe('simple-text');
+  });
+});
+
+describe('unslugify', () => {
+  it('generally does what is expected...', () => {
+    const str = 'this-has-hyphens';
+    expect(unslugify(str)).toEqual('This Has Hyphens');
+  });
+  it('replaces hyphens with spaces', () => {
+    const str = 'this-has-hyphens';
+    expect(unslugify(str).charAt(4)).toEqual(' ');
+    expect(unslugify(str).charAt(8)).toEqual(' ');
+  });
+  it('capitalizes all words', () => {
+    const str = 'this-has-hyphens';
+    expect(unslugify(str).charAt(0)).toEqual('T');
+    expect(unslugify(str).charAt(5)).toEqual('H');
+    expect(unslugify(str).charAt(9)).toEqual('H');
   });
 });
 
